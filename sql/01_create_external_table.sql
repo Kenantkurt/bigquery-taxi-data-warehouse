b6c10bc59e6ba17e74f35614e6154aa3c3b83329
Kenan Kurt <kenantkurt@gmail.com>
2026-04-18 20:03:01 +0200

    Add external table creation SQL

--- sql/01_create_external_table.sql
+++ sql/01_create_external_table.sql
@@ -1,1 +1,5 @@
-
+CREATE OR REPLACE EXTERNAL TABLE `white-set-490418-r8.trips_data_all.yellow_tripdata_external`
+OPTIONS (
+  format = 'PARQUET',
+  uris = ['gs://white-set-490418-r8-taxi-bucket/yellow_taxi_2024/*.parquet']
+);
